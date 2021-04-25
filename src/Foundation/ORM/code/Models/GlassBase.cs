ce5a65d7c1cda334e90cee6498abb5a15e82e61f
Gerhart Magdas <gerhart.magdas@deleteagency.com>
2021-04-25 04:14:46 +0300

    Introduced GlassModel Generations with Leprechaun using Sitecore Content Serialized Items

---
+++ src/Foundation/ORM/code/Models/GlassBase.cs
@@ -0,0 +1,39 @@
+﻿namespace Sitecore.Foundation.ORM.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Glass.Mapper.Sc.Configuration;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Sitecore.Globalization;
+
+    public abstract partial class GlassBase : IGlassBase
+    {
+        [SitecoreId]
+        public virtual Guid Id { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Language)]
+        public virtual Language Language { get; private set; }
+
+        [SitecoreInfo(SitecoreInfoType.Version)]
+        public virtual int Version { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.BaseTemplateIds)]
+        public virtual IEnumerable<Guid> BaseTemplateIds { get; private set; }
+
+        [SitecoreInfo(SitecoreInfoType.TemplateName)]
+        public virtual string TemplateName { get; private set; }
+
+        [SitecoreInfo(SitecoreInfoType.TemplateId)]
+        public virtual Guid TemplateId { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Name)]
+        public virtual string Name { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Url)]
+        public virtual string Url { get; private set; }
+
+        [SitecoreInfo(SitecoreInfoType.FullPath)]
+        public virtual string FullPath { get; private set; }
+    }
+}
---
+++ src/Foundation/ORM/code/Models/IGlassBase.cs
@@ -0,0 +1,19 @@
+﻿namespace Sitecore.Foundation.ORM.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using Sitecore.Globalization;
+
+    public interface IGlassBase
+    {
+        Guid Id { get; set; }
+        Language Language { get; }
+        int Version { get; set; }
+        IEnumerable<Guid> BaseTemplateIds { get; }
+        string TemplateName { get; }
+        Guid TemplateId { get; set; }
+        string Name { get; set; }
+        string Url { get; }
+        string FullPath { get; }
+    }
+}
